a7d6c5a222da1fc05d3aeb3dee6806c22fca98c7
Shrey <shreyansh290103@outlook.com>
2023-10-12 04:36:00 +0100

    Rectify Syntax Error

--- frequencies.py
+++ frequencies.py
@@ -3,7 +3,7 @@
 
 def frequencies(items):
     frequencies = {}
-    for value in items.items():
+    for value in items:
         if value in frequencies.keys():
             frequencies[value] += 1
         else:
