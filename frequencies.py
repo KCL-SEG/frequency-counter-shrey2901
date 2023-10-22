f83e44b339c8697ddd5bed37eb367677dd408c1a
Shrey <shreyansh290103@outlook.com>
2023-10-22 18:47:39 +0100

    Converted the key to string

--- frequencies.py
+++ frequencies.py
@@ -5,5 +5,7 @@ def frequencies(items):
     frequencies = {}
     for value in items:
         if value in frequencies.keys():
-            frequencies[value] += 1
+            frequencies[str(value)] += 1
+        else:
+            frequencies[str(value)] = 1
     return frequencies
